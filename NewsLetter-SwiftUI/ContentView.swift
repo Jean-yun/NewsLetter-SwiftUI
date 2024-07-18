a5ce798d718be52e70bc57076360ce2d24aced26
Jean-yun <yesirchloe@gmail.com>
2024-07-18 16:35:16 +0900

    Initial Commit

---
+++ NewsLetter-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsLetter-SwiftUI
+//
+//  Created by 윤진영 on 7/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsLetter-SwiftUI/NewsLetter_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsLetter_SwiftUIApp.swift
+//  NewsLetter-SwiftUI
+//
+//  Created by 윤진영 on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsLetter_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
